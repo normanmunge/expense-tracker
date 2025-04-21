31e5b8effe2f843d30a70827128e0aeb1f55e699
Norman Munge <normanmunge@gmail.com>
2025-04-21 23:02:24 +0300

    Create mock data

--- constants/mock.ts
+++ constants/mock.ts
@@ -4,41 +4,47 @@ export const DUMMYEXPENSES = [
         description: 'Lunch',
         amount: 1000,
         date: new Date('2025-12-19'),
-        category: 'Food'
+        category: 'Food',
+        mode: 'Mpesa'
     },
     {
         id: 'e2',
         description: 'Fuel',
         amount: 2000,
         date: new Date('2025-12-19'),
-        category: 'Transport'
+        category: 'Transport',
+        mode: 'Cash'
     },
     {
         id: 'e3',
         description: 'Electricity',
         amount: 3000,
         date: new Date('2025-12-19'),
-        category: 'Bills'
+        category: 'Bills',
+        mode: 'Bank Transfer'
     },
     {
         id: 'e4',
         description: 'Gift for Elliot',
         amount: 1000,
         date: new Date('2025-12-19'),
-        category: 'Gifts'
+        category: 'Gifts',
+        mode: 'Mpesa'
     },
     {
         id: 'e5',
         description: 'Gift for Esther',
         amount: 2000,
         date: new Date('2025-12-19'),
-        category: 'Gifts'
+        category: 'Gifts',
+        mode: 'Mpesa'
     },
     {
         id: 'e6',
         description: 'Water Purchase',
         amount: 500,
         date: new Date('2025-12-19'),
-        category: 'Bills'
+        category: 'Bills',
+        mode: 'Mpesa'
     }
 ]
