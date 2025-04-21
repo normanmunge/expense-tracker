e59ba303e3dcfdd5588e3a987eec34ee53349166
Norman Munge <normanmunge@gmail.com>
2025-04-21 22:49:57 +0300

    Update global colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,9 +1,12 @@
 export const COLORS = {
     background: '#1a1a1a',
-    text: '#FFE81F',
+    barText: '#FFE81F',
+    yellow: '#FFE81F',
     inactive: '#888888',
     itemBackground: '#333333',
     containerBackground: '#222222',
     cardBackground: '#F3F3F3',
-    white: '#FFFFFF'
+    white: '#FFFFFF',
+    text: '#3F3F3F',
+    navText: '#575757'
 }
