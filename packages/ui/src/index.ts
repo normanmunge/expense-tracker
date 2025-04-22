3989ccc0c7aebc88dee7e9344008e0641155dcf2
Norman Munge <normanmunge@gmail.com>
2025-04-22 11:21:51 +0300

    Export ui pacakges

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/ui';
+export * from './lib/IconButton';
+export * from './lib/Card';
+export * from './lib/Header';
+export * from './lib/Nav';
+export * from './lib/Button';
+export * from './lib/Title';
