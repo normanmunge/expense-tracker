f45734ae07caa09cb84c114748c58155e8399fda
Norman Munge <normanmunge@gmail.com>
2025-04-23 02:48:50 +0300

    Add tamagui provider wrapper

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -12,79 +13,92 @@ import RecentExpense from "../screens/RecentExpense";
 import AllExpenses from "../screens/AllExpenses";
 import Settings from "../screens/Settings";
 import config from "tamagui.config"
-import { TamaguiProvider } from 'tamagui';
+import { TamaguiProvider, View } from 'tamagui';
+
+import { Plus } from '@tamagui/lucide-icons';
+import { UIButton } from '@expense-app/ui';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 const ExpensesOverview = () => {
     return (
-        <BottomTabs.Navigator screenOptions={({navigation}) => ({
-          headerShadowVisible: false,
-          headerStyle: {
-            backgroundColor: COLORS.background
-          },
-          headerTintColor: COLORS.barText,
-          headerTitleStyle: {
-            fontWeight: 'bold'
-          },
-          tabBarStyle: {
-            backgroundColor: COLORS.containerBackground,
-            borderTopColor: COLORS.barText,
-            borderTopWidth: 1
-          },
-          tabBarActiveTintColor: COLORS.barText,
-          tabBarInactiveTintColor: COLORS.inactive,
-          headerRight: ({tintColor}) => {
-            return (
-              <View></View>
-            // <IconButton 
-            //   icon='add-outline' 
-            //   size={24} 
-            //   color={tintColor} 
-            //   onPress={() => {
-            //     navigation.navigate('Manage Expense')
-            //   }}
-            // />
-            )
-          }
-        })}>
-           <BottomTabs.Screen 
-              name="All Expenses" 
-              component={AllExpenses} 
-              options={{
-                tabBarIcon: ({color, size}) => (
-                  <Ionicons name="home-outline" size={size} color={color} />
-                )
-              }}
+      <BottomTabs.Navigator screenOptions={({navigation}) => ({
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: COLORS.containerBackground
+        },
+        headerTintColor: COLORS.barText,
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        },
+        tabBarStyle: {
+          backgroundColor: COLORS.containerBackground,
+          borderTopColor: COLORS.barText,
+          borderTopWidth: 1
+        },
+        tabBarActiveTintColor: COLORS.barText,
+        tabBarInactiveTintColor: COLORS.inactive,
+        headerRight: ({tintColor}) => {
+          return (
+            <View 
+              bc={COLORS.barText} 
+              w={'$3'} 
+              h={'$3'} 
+              br={'100%'} 
+              justifyContent="center"
+              alignItems="center"
+              marginRight={'$4'}
+            >
+               <UIButton 
+                icon={Plus} 
+                size={'$9'} 
+                color={COLORS.barText} 
+                iconify 
+                onPress={() => {
+                  navigation.navigate('Manage Expense')
+                }}
             />
-            <BottomTabs.Screen
-              name="Recent Expenses" 
-              component={RecentExpense} 
-              options={{
-                  tabBarIcon: ({color, size}) => (
-                    <Ionicons name="calendar-outline" size={size} color={color} />
-                  ),
-                  
-                }} 
-              /> 
-            <BottomTabs.Screen 
-              name="Settings" 
-              component={Settings} 
-              options={{
+            </View>
+          )
+        }
+      })}>
+          <BottomTabs.Screen 
+            name="All Expenses" 
+            component={AllExpenses} 
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="home-outline" size={size} color={color} />
+              )
+            }}
+          />
+          <BottomTabs.Screen
+            name="Recent Expenses" 
+            component={RecentExpense} 
+            options={{
                 tabBarIcon: ({color, size}) => (
-                  <Ionicons name="cog-outline" size={size} color={color} />
-                )
-              }}
-            />
-        </BottomTabs.Navigator>
+                  <Ionicons name="calendar-outline" size={size} color={color} />
+                ),
+                
+              }} 
+            /> 
+          <BottomTabs.Screen 
+            name="Settings" 
+            component={Settings} 
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Ionicons name="cog-outline" size={size} color={color} />
+              )
+            }}
+          />
+      </BottomTabs.Navigator>
     )
 } 
 
 export default function App() {
   return (
     <TamaguiProvider config={config}>
-      <StatusBar />
+      <StatusBar style="light" />
       <SafeAreaView
         style={{
           flex: 1,
