2a250518c4a3826da665013289ff1dfb6cf4f791
Norman Munge <normanmunge@gmail.com>
2025-04-25 14:22:23 +0300

    Create add expense component

---
+++ apps/mobile/src/components/AddExpense.tsx
@@ -0,0 +1,25 @@
+import { View, YStack, Text, Button } from 'tamagui'
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = { 'Manage Expense': undefined}
+
+const AddExpense = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+    return (
+        <View>
+            <YStack jc='center' ac='center' gap='$5'>
+                <Text textAlign='center'>You currently have no expenses 😊 </Text>
+                <Button onPress={() => {
+                    console.log('navigate to manage expense')
+                  navigation.navigate('Manage Expense')
+                }}>
+                    Add Expense
+                </Button>
+            </YStack>
+        </View>
+    )
+}
+
+export default AddExpense;
