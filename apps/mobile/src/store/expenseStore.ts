0572f95891299600e61dc9d50b192e6946225668
Norman Munge <normanmunge@gmail.com>
2025-04-25 14:22:13 +0300

    Integrate mmkv for persistent storage

--- apps/mobile/src/store/expenseStore.ts
+++ apps/mobile/src/store/expenseStore.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import { zustandStorage } from './mmkv';
 import { Expense } from '../types';
-import { transactions } from '../mock'
 
 export interface ExpenseState {
     expenses: Array<Expense>;
@@ -10,38 +11,43 @@ export interface ExpenseState {
     totalExpense: () => string;
 }
 
-const useExpenseStore = create<ExpenseState>()((set, get) => ({
-    expenses: transactions,
-    addExpense: (expense: Expense) => set((state) => {
-        return {
-            expenses: [...state.expenses, expense]
-        }
-    }),
-    deleteExpense: (expense: Expense) => set((state) => {
-        return {
-            expenses: state.expenses.filter(exp => exp.id === expense.id)
-        }
-    }),
-    updateExpense: (
-        id: string, 
-        values: Omit<Expense, 'id'>
-    ) => set((state) => {
+const useExpenseStore = create<ExpenseState>()(
+    persist(
+    (set, get) => ({
+        expenses: [],
+        addExpense: (expense: Expense) => set((state) => {
             return {
-                expenses: state.expenses.map(exp => {
-                    if(exp.id === id) {
-                        exp.name = values.name
-                        exp.amount = values.amount
-                        exp.date = values.date
-                        exp.paymentMethod = values.paymentMethod
-                        exp.category = values.category
-                        exp.description = values.description
-                    }
-                    return exp;
-                })
+                expenses: [...state.expenses, expense]
             }
-    }),
-    totalExpense: () => get().expenses.reduce((acc, exp) => acc + exp.amount, 0).toFixed(2)
-    }),
+        }),
+        deleteExpense: (expense: Expense) => set((state) => {
+            return {
+                expenses: state.expenses.filter(exp => exp.id === expense.id)
+            }
+        }),
+        updateExpense: (
+            id: string, 
+            values: Omit<Expense, 'id'>
+        ) => set((state) => {
+                return {
+                    expenses: state.expenses.map(exp => {
+                        if(exp.id === id) {
+                            exp.name = values.name
+                            exp.amount = values.amount
+                            exp.date = values.date
+                            exp.paymentMethod = values.paymentMethod
+                            exp.category = values.category
+                            exp.description = values.description
+                        }
+                        return exp;
+                    })
+                }
+        }),
+        totalExpense: () => get().expenses.reduce((acc, exp) => acc + exp.amount, 0).toFixed(2)
+    }), {
+        name: 'expense-app',
+        storage: createJSONStorage(() => zustandStorage),
+    })
 )
 
 export default useExpenseStore;
