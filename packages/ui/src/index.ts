8cb20b6fc43a9cf5364a722f9c42803cba45d71c
Norman Munge <normanmunge@gmail.com>
2025-04-28 06:58:10 +0300

    Update tamagui configuration

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,17 +1,5 @@
-// export * from './lib/IconButton';
-// export * from './lib/Card';
-// export * from './lib/Header';
-// export * from './lib/Nav';
-// export * from './lib/Button';
-// export * from './lib/Title';
-
 export { default as tamaguiConfig } from './tamagui.config';
-export { UIButton } from './lib/Button';
-export { UITitle } from './lib/Text';
-export { UIView } from './lib/View';
-export { UIInput } from './lib/Input'
-
-
-//export type { AppConfig } from './tamagui.config';
-
-// export { Button } from 'components/Button';
+export * from './lib/Button';
+export * from './lib/Text';
+export * from './lib/View';
+export * from './lib/Input'
--- packages/ui/src/tamagui.config.ts
+++ packages/ui/src/tamagui.config.ts
@@ -1,31 +1,57 @@
-import { createTamagui } from 'tamagui';
-import { createInterFont } from '@tamagui/font-inter';
-
+import { createTamagui, createTokens } from 'tamagui';
 import { config } from '@tamagui/config/v2';
 
-const headingFont = createInterFont()
-const bodyFont = createInterFont()
+const tokens = createTokens({
+  ...config.tokens,
+  color: {
+    primary: '#0056B3',
+    secondary: '#FFE81F',
+    background: '#000000',
+    text: '#3F3F3F',
+    success: '#34C759',
+    error: '#FF3B30',
+    warning: '#FF9500',
+    white: '#FFFFFF',
+    gray100: '#F2F2F7',
+    gray200: '#E5E5EA',
+    gray300: '#D1D1D6',
+    gray500: '#F3F3F3',
+    gray900: '#888888',
+  },
+  space: {
+    ...config.tokens.space,
+    true: 1,
+  },
+  size: {
+    ...config.tokens.size,
+    true: 1,
+  },
+  radius: {
+    ...config.tokens.radius,
+    true: 1,
+  },
+  zIndex: {
+    ...config.tokens.zIndex,
+    true: 1,
+  },
+});
 
-// const config = createTamagui({
-//   fonts: {
-//     heading: headingFont,
-//     body: bodyFont,
-//   },
-//   themes: {
-//     light: {
-//       background: 'white',
-//       foreground: 'black',
-//     },
-//     dark: {
-//       background: 'black',
-//       foreground: 'white',
-//     },
-//   }
-// })
-
-//export type AppConfig = typeof tamaguiConfig;
-
-const tamaguiConfig = createTamagui(config)
+const tamaguiConfig = createTamagui({
+  ...config,
+  tokens,
+  themes: {
+    light: {
+      background: 'color.$background',
+      // color: '$background',
+      // primary: '$secondary',
+    },
+    dark: {
+      background: 'color.$background',
+      // color: '$text',
+      // primary: '$primary'
+    },
+  },
+});
 
 type Conf = typeof tamaguiConfig;
 
