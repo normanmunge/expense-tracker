4011071d5eab3d568c56b07e97b2d0c5cf7d5ecd
Norman Munge <normanmunge@gmail.com>
2025-04-30 11:09:47 +0300

    Update button component

--- packages/ui/src/lib/Button.tsx
+++ packages/ui/src/lib/Button.tsx
@@ -1,64 +1,134 @@
-import { styled, Button, GetProps } from "tamagui";
+import { 
+    styled, 
+    Stack, 
+    Text, 
+    GetProps, 
+    createStyledContext, 
+    SizeTokens, 
+    ColorTokens,
+    useTheme, 
+    withStaticProperties, 
+    getTokens 
+} from "tamagui";
+import { getSize } from '@tamagui/get-token'
+import { cloneElement, ReactElement, useContext } from 'react'
 
-export const UIButton = styled(Button, {
-    name: 'UIButton',
+export const ButtonContext = createStyledContext({
+    size: '$md' as SizeTokens,
+    color: '$color' as ColorTokens
+})
+
+export const ButtonFrame = styled(Stack, {
+    name: 'Button',
+    context: ButtonContext,
+    backgroundColor: '$background',
+    borderRadius: '$sm',
+    borderWidth: 1,
+    borderColor: '$background',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    pressStyle: {
+        backgroundColor: '$gray900'
+    },
     variants: {
-        iconify: {
+        size: {
+            '...size': (name) => {
+                const size = getSize(name)
+                return {
+                    height: size,
+                    borderRadius: Number(size) / 2,
+                    gap: Number(size) * 0.2,
+                    paddingVertical: '$5'
+                }
+            }
+        },
+        outlined: {
             true: {
                 backgroundColor: 'transparent',
-                borderWidth: 0,
-                padding: 0,
-                margin: 0,
-                borderRadius: 0,
-                borderColor: 'transparent',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'row',
-                gap: 0,
-                width: 'auto',
-                height: 'auto',
                 color: '$color',
-                size: '$9',
-                
+                borderColor: 'transparent',
+                pressStyle: {
+                    backgroundColor: 'transparent'
+                }
             }
+        },
+        solid: {
+            true: {
+                backgroundColor: '$primary',
+                color: '$color.white'
+            }
+        },
+        solid_dark: {
+            backgroundColor: 'transparent',
+            outlineColor:'$gray900',
         },
         rounded: {
-            true: {
-                br: '100%',
-                bc: '$primary',
-                color: '$color',
-                size: '$9',
-                w: '$9',
-                h: '$9',
-            }
-        },
-        defaultDark: {
-            true: {
-                backgroundColor: '$white',
-                borderRadius: '$2',
-                color: '$white',
-                borderWidth: 1,
-                paddingVertical: 5,
-                size:'$4'
-            }
-        },
-        isSocialProviderButton: {
-            true: {
-                backgroundColor: 'transparent',
-                borderRadius: '$2',
-                color: '$white',
-                borderColor: '$gray900',
-                outlineColor:'$gray900',
-                borderWidth: 1,
-                paddingVertical: 5
-            }
+            br: '100%',
+            bc: '$primary',
+            color: '$color',
+            size: '$9',
+            w: '$9',
+            h: '$9',
         }
     } as const,
     defaultVariants: {
-        iconify: false,
-        rounded: false,
-        isSocialProviderButton: false
+        size: '$md',
+        outlined: false
     }
 })
 
 export type UIButtonProps = GetProps<typeof UIButton>
+
+export const StyledButtonText = styled(Text, {
+    name: 'Text',
+    context: ButtonContext,
+    color: '$color',
+    paddingHorizontal: 10,
+    fontFamily: '$body',
+    userSelect: 'none',
+    variants: {
+        size: {
+            '...size': (name) => {
+                const size = getSize(name)
+                return {
+                    fontSize: size ? Number(size) * 0.35 : 14,
+                }
+            }
+        },
+    } as const,
+    defaultVariants: {
+        size: '$md'
+    }
+})
+
+type ButtonTextProps = GetProps<typeof StyledButtonText> & {
+    children: React.ReactNode;
+}
+
+export const ButtonText = ({ children, ...props }: ButtonTextProps) => {
+    return <StyledButtonText {...props}>{children}</StyledButtonText>;
+}
+
+const ButtonIcon = (props: { icon: React.ComponentType<any>, color?: string }) => {
+    
+    const { size } = useContext(ButtonContext)
+    const smallerSize = Number(getSize(size)) * 0.2
+    const theme = useTheme()
+    const Icon = props.icon
+
+    return (
+        <Icon 
+            width={smallerSize} 
+            height={smallerSize} 
+            color={props.color || theme.color.get()} 
+            backgroundColor={'transparent'}
+        />
+    )
+}
+
+export const UIButton = withStaticProperties(ButtonFrame, {
+    Props: ButtonContext.Provider,
+    Text: ButtonText,
+    Icon: ButtonIcon,
+})
