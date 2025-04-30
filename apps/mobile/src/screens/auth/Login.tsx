106f9bc9ffe284446ea74a654a6dc3c7f32059d5
Norman Munge <normanmunge@gmail.com>
2025-05-01 00:29:42 +0300

    Update login screen

--- apps/mobile/src/screens/auth/Login.tsx
+++ apps/mobile/src/screens/auth/Login.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import { Form, Label, Input, YStack, Paragraph, XStack, Stack, Text, Separator } from "tamagui"
+import { Form, Label, Input, YStack, Paragraph, XStack, Stack, Text, Separator, Theme } from "tamagui"
 import { useForm, Controller, SubmitHandler } from 'react-hook-form'
 import { z } from "zod"
 import { zodResolver } from '@hookform/resolvers/zod';
-import { UIButton, UITitle, UIView } from "@expense-app/ui"
-import { Eye, EyeOff, Github, Chrome } from '@tamagui/lucide-icons';
+import { UIButton, UIInput, UITitle, UIView } from "@expense-app/ui"
+import { Eye, EyeOff, Github, Chrome, Apple } from '@tamagui/lucide-icons';
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Alert, ActivityIndicator } from 'react-native';
@@ -55,116 +55,136 @@ const Login = () => {
     })
     
     return (
-        <UIView isAuthBackgroundContainer>
-            <YStack>
-                <Stack marginBottom={'$5'}>
-                    <UITitle isHeading>Log In</UITitle>
-                </Stack>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <YStack gap="$4">
-                        <YStack>
-                            <Label color={'$color.white'} fontSize={14}>Email</Label>
-                            <Controller
-                                control={control}
-                                name="email"
-                                render={({ field}) => (
-                                    <Input
-                                        size="$4"
-                                        placeholder="Email Address"
-                                        placeholderTextColor={'$color.text'}
-                                        value={field.value.toString()}
-                                        onChangeText={field.onChange}
-                                        borderRadius={4}
-                                        borderWidth={1}
-                                        borderColor={'$color.gray900'}
-                                        backgroundColor={'$color.background'}
-                                        color={'$color.white'}
-                                    />
-                                )}
-                            />
-                            {errors.email && <Paragraph color={'$color.error'}>{errors.email.message}</Paragraph>}
-                        </YStack>
+        <UIView.Props>
+            <UIView fullPage>
+                <UIView.Content>
+                    <UIView.Heading size='lg'>
+                        Log In
+                    </UIView.Heading>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <YStack gap={16}>
+                        <UIInput<LoginFormData>
+                            control={control}
+                            name='email'
+                            label='Email'
+                            placeholder='Enter email'
+                            keyboardType='email-address'
+                            defaultValue={''}
+                        />
 
-                        <YStack>
-                            <Label color={'$color.white'} fontSize={14}>Password</Label>
-                            <XStack borderRadius={4}
-                                borderWidth={1}
-                                borderColor={'$color.gray900'}
-                                backgroundColor={'$color.background'}
-                            >
+                        <UIInput<LoginFormData>
+                            control={control}
+                            name='password'
+                            label='Password'
+                            placeholder='Enter password'
+                            keyboardType='default'
+                            secureTextEntry={showPassword}
+                            defaultValue={''}
+                        />
+                            {/* <YStack>
+                                <Label color={'$color.white'} fontSize={14}>Email</Label>
                                 <Controller
                                     control={control}
-                                    name="password"
+                                    name="email"
                                     render={({ field}) => (
                                         <Input
-                                            flex={1}
                                             size="$4"
-                                            placeholder="Password"
+                                            placeholder="Email Address"
                                             placeholderTextColor={'$color.text'}
                                             value={field.value.toString()}
                                             onChangeText={field.onChange}
-                                            secureTextEntry={!showPassword}
-                                            color={'$color.white'}
+                                            borderRadius={4}
+                                            borderWidth={1}
+                                            borderColor={'$color.gray900'}
                                             backgroundColor={'$color.background'}
-                                            borderWidth={0}
+                                            color={'$color.white'}
                                         />
                                     )}
                                 />
-                                <UIButton
-                                    iconify
+                                {errors.email && <Text color={'$color.error'}>{errors.email.message}</Text>}
+                            </YStack> */}
+
+                            {/* <YStack>
+                                <Label color={'$color.white'} fontSize={14}>Password</Label>
+                                <XStack borderRadius={4}
+                                    borderWidth={1}
+                                    borderColor={'$color.gray900'}
                                     backgroundColor={'$color.background'}
-                                    color={'$color.white'}
-                                    size={'$4'}
-                                    marginRight={'$4'}
-                                    icon={showPassword ? EyeOff : Eye}
-                                    onPress={() => setShowPassword(!showPassword)}
-                                />
+                                >
+                                    <Controller
+                                        control={control}
+                                        name="password"
+                                        render={({ field}) => (
+                                            <Input
+                                                flex={1}
+                                                size="$4"
+                                                placeholder="Password"
+                                                placeholderTextColor={'$color.text'}
+                                                value={field.value.toString()}
+                                                onChangeText={field.onChange}
+                                                secureTextEntry={!showPassword}
+                                                color={'$color.white'}
+                                                backgroundColor={'$color.background'}
+                                                borderWidth={0}
+                                            />
+                                        )}
+                                    />
+                                    <UIButton outlined size='$sm' onPress={() => setShowPassword(!showPassword)}>
+                                        <UIButton.Icon icon={showPassword ? Eye : EyeOff} />
+                                    </UIButton>
+                                </XStack>
+                                {errors.password && <Paragraph color={'$color.error'}>{errors.password.message}</Paragraph>}
+                            </YStack> */}
+
+                            <XStack justifyContent='space-between'>
+                                <Paragraph 
+                                    color={'$color.secondary'}
+                                >Forgot Password?</Paragraph>
+                                <Paragraph color={'$color.white'} onPress={() => {
+                                    navigation.navigate('signup')
+                                }}>Create Account</Paragraph>
                             </XStack>
-                            {errors.password && <Paragraph color={'$color.error'}>{errors.password.message}</Paragraph>}
+
+                            <Form.Trigger asChild disabled={!isValid || isSubmitting}>
+                                <Stack>
+                                    <UIButton size={'$md'} onPress={handleSubmit(onSubmit)}>
+                                        <UIButton.Text>
+                                            {isSubmitting ? 'Logging In...' : 'Login'}
+                                        </UIButton.Text>
+                                    </UIButton>
+                                </Stack>
+                            </Form.Trigger>
                         </YStack>
-
-                        <XStack justifyContent='space-between'>
-                            <Paragraph 
-                                color={'$color.secondary'}
-                            >Forgot Password?</Paragraph>
-                            <Paragraph color={'$color.white'} onPress={() => {
-                                navigation.navigate('signup')
-                            }}>Create Account</Paragraph>
+                    </Form>
+                    
+                    <YStack mt={30}>
+                        <XStack>
+                            <XStack flex={1} justifyContent='center' alignItems='center'>
+                                <Separator borderWidth={1} borderColor={'$color.gray900'} />
+                                    <Text color={'$color.white'} paddingHorizontal={10} >OR</Text>
+                                <Separator borderWidth={1} borderColor={'$color.gray900'} />
+                            </XStack>
                         </XStack>
-
-                        <Form.Trigger asChild disabled={!isValid || isSubmitting}>
-                            <Stack>
-                                <UIButton defaultDark  size={'$4'} marginVertical={'0'} color={'$color.background'}>
-                                    <Paragraph>
-                                        { isSubmitting ? 'Logging In...': 'Login'}
-                                    </Paragraph>
-                                </UIButton>
-                            </Stack>
-                        </Form.Trigger>
-                    </YStack>
-                </Form>
-                
-                <YStack marginTop={'$4'}>
-                    <XStack>
-                        <XStack flex={1} justifyContent='center' alignItems='center'>
-                            <Separator borderWidth={1} borderColor={'$color.gray900'} />
-                                <Text color={'$color.white'} paddingHorizontal={'$4'} >OR</Text>
-                            <Separator borderWidth={1} borderColor={'$color.gray900'} />
-                        </XStack>
-                    </XStack>
-                    <YStack marginTop={'$4'}>
-                        <YStack gap={16}>
-                            <Stack>
-                                <UIButton iconify icon={Github} isSocialProviderButton>Continue with Github</UIButton>
-                            </Stack>
-                            <Stack>
-                                <UIButton iconify icon={Chrome} isSocialProviderButton>Continue with Google</UIButton>
-                            </Stack>
+                        <YStack mt={30}>
+                            <YStack gap={16}>
+                                <Stack>
+                                    <UIButton size={'$md'}>
+                                        <UIButton.Icon icon={Apple} />
+                                        <UIButton.Text>Continue with Apple</UIButton.Text>
+                                    </UIButton>
+                                </Stack>
+                                <Stack>
+                                    <UIButton size={'$md'}>
+                                        <UIButton.Icon icon={Chrome} />
+                                        <UIButton.Text>Continue with Google</UIButton.Text>
+                                    </UIButton>
+                                </Stack>
+                            </YStack>
                         </YStack>
                     </YStack>
-                </YStack>
-            </YStack>
-        </UIView>
+                </UIView.Content>
+            </UIView>
+        </UIView.Props>
     )
 }
 
